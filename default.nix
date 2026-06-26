cfdae44004725a7c140394720d12c5f7549a8f31
Ahmed Shahir <ahmdsphere+git@gmail.com>
2026-06-26 15:51:35 +0600

    Fix missing hash in default.nix

--- default.nix
+++ default.nix
@@ -2,8 +2,12 @@
 let
   lock = builtins.fromJSON (builtins.readFile ./flake.lock);
   rev = lock.nodes.nixpkgs.locked.rev;
+  narHash = lock.nodes.nixpkgs.locked.narHash;
 in
-import (fetchTarball "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz") ({
+import (fetchTarball {
+  url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
+  sha256 = narHash;
+}) ({
   inherit system;
   config = { allowUnfree = true; };
 })
