a44acd1dce8697708033405cf5d195a7b1efe3f1
Ahmed Shahir <ahmdsphere+git@gmail.com>
2026-07-04 21:09:18 +0600

    Fix default.nix system (again)

--- default.nix
+++ default.nix
@@ -3,15 +3,10 @@ let
   lock = builtins.fromJSON (builtins.readFile ./flake.lock);
   rev = lock.nodes.nixpkgs.locked.rev;
   narHash = lock.nodes.nixpkgs.locked.narHash;
-  system = 
-    if args ? system then args.system
-    else if builtins ? currentSystem then builtins.currentSystem
-    else null;
 in
 import (fetchTarball {
   url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
   sha256 = narHash;
-}) ({
-  inherit system;
-  config = { allowUnfree = true; };
+}) (args // {
+  config = (args.config or { }) // { allowUnfree = true; };
 })
